428d9b358d5f8f2cd05a24e16b18e2918ce4733b
Daniel Balastegui <dbalas@MacBook-Air.local>
2017-09-13 08:59:26 +0200

    fixed typo

--- send.js
+++ send.js
@@ -2,4 +2,4 @@ const bot = require('./Telebot')
 
 const ID = -211104509
 
-bot.sendMessage(ID, 'Hola mortales, quería comentaros que me han violado (actualizado) una vez más, ahora cojo farlopa de Reddit aunque priorizo 9GAG por que es más pura. Tambien deciros que el roñoso de mi creador ha creado un link para que le deis viruta, hay que mantener mi casa y esas cosas. ¡Chao pescao!')
+bot.sendMessage(ID, 'Hola mortales, quería comentaros que me han violado (actualizado) una vez más, ahora cojo farlopa de Reddit aunque priorizo 9GAG por que es más pura. También deciros que el roñoso de mi creador ha creado un link para que le deis viruta, hay que mantener mi casa y esas cosas. ¡Chao pescao!')
